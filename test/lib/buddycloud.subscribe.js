f6fa68e5eb87348710da06fc6946cb56fb9c5957
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-03-11 11:50:38 +0000

    Add same code for subscribing

---
+++ test/lib/buddycloud.subscribe.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var Buddycloud = require('../../index')
+  , helper  = require('../helper')
+
+/* jshint -W030 */
+describe('Subscribe', function() {
+
+    var buddycloud, socket, xmpp, manager
+
+    before(function() {
+        socket = new helper.SocketEventer()
+        xmpp = new helper.XmppEventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            },
+            jid: 'juliet@capulet.lit',
+            _getLogger: function() {
+                return {
+                    log: function() {},
+                    info: function() {},
+                    error: function() {},
+                    warn: function() {}
+                }
+            }
+        }
+        buddycloud = new Buddycloud()
+        buddycloud.init(manager)
+    })
+
+    beforeEach(function() {
+        socket.removeAllListeners()
+        xmpp.removeAllListeners()
+        buddycloud.init(manager)
+        buddycloud.channelServer = 'channels.example.com'
+    })
+
+    it('Overwrites JID with bare JID', function(done) {
+        var request = {
+            jid: 'juliet@capulet.lit/balcony',
+            to: 'channels.capulet.lit',
+            node: '/user/romeo@montague.lit/posts'
+        }
+        xmpp.once('stanza', function(stanza) {
+            stanza.is('iq').should.be.true
+            stanza.attrs.to.should.equal(request.to)
+            stanza.attrs.type.should.equal('set')
+            var pubsub = stanza.getChild('pubsub', buddycloud.NS_PUBSUB)
+            pubsub.should.exist
+            var unsubscribe = pubsub.getChild('subscribe')
+            unsubscribe.should.exist
+            unsubscribe.attrs.node.should.equal(request.node)
+            unsubscribe.attrs.jid.should.equal(manager.jid)
+            done()
+        })
+        socket.send('xmpp.buddycloud.subscribe', request, function() {})
+    })
+
+})
